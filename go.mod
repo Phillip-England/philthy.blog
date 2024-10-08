753f7ab7b4fff4239a1095d27a84f667ce0f6f88
Phillip-England <phillip.mark.england@gmail.com>
2024-10-08 06:39:22 -0500

    transitioned from templates to string-based components

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module philthy.blog
 
 go 1.23.2
 
+require github.com/Phillip-England/vbf v0.0.14
+
 require (
 	github.com/Phillip-England/ffh v1.0.11 // indirect
-	github.com/Phillip-England/vbf v0.0.12 // indirect
 	github.com/a-h/templ v0.2.771 // indirect
 	github.com/alecthomas/chroma/v2 v2.14.0 // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"strings"
@@ -42,20 +43,13 @@ func main() {
 	// Define the route with middleware and template execution
 	vbf.AddRoute("GET /", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
-			t, _ := vbf.GetContext(KEY_TEMPLATES, r).(*template.Template)
-			mdContent, err := vbf.LoadMarkdown("./static/docs/content.md")
+			mdContent, err := vbf.LoadMarkdown("./static/docs/me.md")
 			if err != nil {
 				vbf.WriteString(w, "failed to load markdown content")
 				return
 			}
-			styledMdContent := StyleMarkdownContent(mdContent)
-			err = vbf.ExecuteTemplate(w, t, "layout.html", map[string]interface{}{
-				"Title":       "Why am I Writing?",
-				"HeaderText":  "Philthy Blog",
-				"SubText":     "Saying the things I'm afraid to say",
-				"Content":     template.HTML(styledMdContent),
-				"CurrentPath": r.URL.Path,
-			})
+			mdContent = StyleMarkdownContent(mdContent)
+			vbf.WriteHTML(w, Layout("Philthy", "Philthy", "Saying the things I'm afraid to say", r.URL.Path, mdContent))
 			if err != nil {
 				vbf.WriteString(w, err.Error())
 				return
@@ -69,9 +63,31 @@ func main() {
 		t, _ := vbf.GetContext(KEY_TEMPLATES, r).(*template.Template)
 		err = vbf.ExecuteTemplate(w, t, "layout.html", map[string]interface{}{
 			"Title":       "Posts",
-			"HeaderText":  "Philthy Blog",
+			"HeaderText":  "Philthy",
 			"SubText":     "Saying the things I'm afraid to say",
 			"Content":     "",
+			"CurrentPath": r.URL.Path,
+		})
+		if err != nil {
+			vbf.WriteString(w, err.Error())
+			return
+		}
+	}, vbf.MwLogger)
+
+	vbf.AddRoute("GET /posts/{postNumber}", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
+		postNumber := r.PathValue("postNumber")
+		mdContent, err := vbf.LoadMarkdown(fmt.Sprintf("./static/docs/%s.md", postNumber))
+		mdContent = StyleMarkdownContent(mdContent)
+		if err != nil {
+			vbf.WriteString(w, "failed to load markdown content")
+			return
+		}
+		t, _ := vbf.GetContext(KEY_TEMPLATES, r).(*template.Template)
+		err = vbf.ExecuteTemplate(w, t, "layout.html", map[string]interface{}{
+			"Title":       "Posts",
+			"HeaderText":  "Philthy",
+			"SubText":     "Saying the things I'm afraid to say",
+			"Content":     template.HTML(mdContent),
 			"CurrentPath": r.URL.Path,
 		})
 		if err != nil {
@@ -97,8 +113,8 @@ func ParseTemplatesWithFuncs(templateDir string, funcMap template.FuncMap) (*tem
 	return parsedTmpl, nil
 }
 
+// takes plain markdown HTML and adds tailwind classes for styling
 func StyleMarkdownContent(mdContent string) string {
-	// Apply styles to the Markdown content
 	mdContent = strings.ReplaceAll(mdContent, "<p><em>", "<p class='text-xs italic'><em>")
 	mdContent = strings.ReplaceAll(mdContent, "<h1>", "<h1 class='font-bold text-2xl pt-4'>")
 	mdContent = strings.Replace(mdContent, "<h1 class='font-bold text-2xl pt-4'>", "<h1 class='font-bold text-2xl'>", 1)
@@ -106,6 +122,97 @@ func StyleMarkdownContent(mdContent string) string {
 	mdContent = strings.Replace(mdContent, "<h2 class='font-bold text-xl pt-4'>", "<h2 class='font-bold text-xl'>", 1)
 	mdContent = strings.ReplaceAll(mdContent, "<ol>", "<ol class='list-decimal list-inside pl-4 flex flex-col gap-2'>")
 	mdContent = strings.ReplaceAll(mdContent, "<img", "<img class='pt-4 w-fit md:max-h-sm md:max-w-sm'")
-
 	return mdContent
+}
+
+//==================================
+// COMPONENTS
+//==================================
+
+func Layout(title string, headerText string, subText string, currentPath string, mdContent string) string {
+	return fmt.Sprintf(`
+		<html lang="en">
+	    <head>
+	        <meta charset="UTF-8" />
+	        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	        <link rel="stylesheet" href="/static/css/output.css" />
+	        <script src="/static/js/index.js"></script>
+	        <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
+	        <title>%s | Philthy</title>
+	    </head>
+	    <body class="h-screen">
+	        <div id="root" class="min-h-screen">
+	            <div class="h-[95px]"></div>
+					%s%s
+	            <main
+	                class="flex-grow p-4 flex flex-col gap-4 md:pl-[300px]"
+	                style="min-height: calc(100vh - 190px)"
+	            >
+	                <div class="flex flex-col md:pl-12 md:p-8 gap-4">
+	                    %s
+	                </div>
+	            </main>
+	            %s
+	            <div class="h-[100px]"></div>
+	        </div>
+	    </body>
+	</html>
+	`, title, Header(headerText, subText), NavMenu(currentPath), mdContent, Overlay())
+}
+
+func Overlay() string {
+	return `
+		<div id="overlay" _="on click toggle .hidden on me then toggle .hidden on #icon-bars then toggle .hidden on #icon-x then toggle .hidden on #navmenu" class="h-full w-full bg-black opacity-50 fixed top-0 hidden z-30"></div>
+	`
+}
+
+func Header(headerText string, subText string) string {
+	return fmt.Sprintf(`
+		<header class="flex flex-row justify-between p-4 border-b select-none z-40 bg-white fixed w-full top-0 h-[95px]">
+    		<img src="/static/img/logo.svg" />
+		    <div class="flex flex-col gap-2">
+		        <h1 class="font-bold text-2xl">%s</h1>
+		        <p class="text-sm">%s</p>
+		    </div>
+		    <div class="flex items-center">
+		        <div id="icon-bars" _="on click toggle .hidden on me then toggle .hidden on #icon-x then toggle .hidden on #overlay then toggle .hidden on #navmenu" class="md:hidden">
+		            <svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+		                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
+		            </svg>
+		        </div>
+		        <div id="icon-x" class="hidden md:hidden" _="on click toggle .hidden on me then toggle .hidden on #icon-bars then toggle .hidden on #overlay then toggle .hidden on #navmenu">
+		            <svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+		                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
+		            </svg>
+		        </div>
+		    </div>
+		</header>
+	`, headerText, subText)
+}
+
+func NavMenu(currentPath string) string {
+	return fmt.Sprintf(`
+		<nav id="navmenu" class="flex top-[95px] fixed bg-white w-1/3 h-full border-r z-40 hidden md:flex md:w-[300px]">
+		    <ul class="flex flex-col gap-2 p-2 w-full text-sm">
+				%s%s
+		    </ul>
+		</nav>
+
+	`, NavItem(currentPath, "/", "Home"), NavItem(currentPath, "/posts", "Posts"))
+}
+
+func NavItem(currentPath string, href string, text string) string {
+	if currentPath == href {
+		return fmt.Sprintf(`
+		  <li class="rounded border w-full flex bg-gray-200">
+		      <a class="w-full p-4" href="%s">%s</a>
+		  </li>
+		`, href, text)
+	} else {
+		return fmt.Sprintf(`
+		  <li class="rounded border w-full flex bg-white">
+		      <a class="w-full p-4" href="%s">%s</a>
+		  </li>
+		`, href, text)
+	}
 }
