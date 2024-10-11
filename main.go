98a4a43193d6d63aa1e747e7151c5b02565c5ff5
Phillip-England <phillip.mark.england@gmail.com>
2024-10-10 19:12:55 -0500

    got SocialMediaBanner sticking to top of layout

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"strings"
 
 	"github.com/Phillip-England/vbf"
 )
-
-const KEY_TEMPLATES = "TEMPLATES"
 
 func main() {
 
@@ -17,22 +14,7 @@ func main() {
 	// INIT
 	//==================================
 
-	// Define the global FuncMap with the custom "eq" function
-	funcMap := template.FuncMap{
-		"eq": func(a, b string) bool { return a == b },
-	}
-
-	// Parse templates and apply the FuncMap to all templates
-	templates, err := ParseTemplatesWithFuncs("./templates", funcMap)
-	if err != nil {
-		panic(err)
-	}
-
 	mux, gCtx := vbf.VeryBestFramework()
-
-	// Store the parsed templates in the global context
-	vbf.SetGlobalContext(gCtx, KEY_TEMPLATES, templates)
-
 	vbf.HandleStaticFiles(mux)
 	vbf.HandleFavicon(mux)
 
@@ -55,83 +37,53 @@ func main() {
 		}
 	}, vbf.MwLogger)
 
-	vbf.AddRoute("GET /about", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-		mdContent, err := vbf.LoadMarkdown("./static/docs/about.md")
+	vbf.AddRoute("GET /posts", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
+		mdContent, err := vbf.LoadMarkdown("./static/docs/home.md")
 		if err != nil {
 			vbf.WriteString(w, "failed to load markdown content")
 			return
 		}
 		mdContent = StyleMarkdownContent(mdContent)
 		vbf.WriteHTML(w, Layout("Philthy", "Philthy", "Saying the things I'm afraid to say", r.URL.Path, mdContent))
-		if err != nil {
-			vbf.WriteString(w, err.Error())
-			return
-		}
 	}, vbf.MwLogger)
 
-	vbf.AddRoute("GET /posts", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-		t, _ := vbf.GetContext(KEY_TEMPLATES, r).(*template.Template)
-		err = vbf.ExecuteTemplate(w, t, "layout.html", map[string]interface{}{
-			"Title":       "Posts",
-			"HeaderText":  "Philthy",
-			"SubText":     "Saying the things I'm afraid to say",
-			"Content":     "",
-			"CurrentPath": r.URL.Path,
-		})
-		if err != nil {
-			vbf.WriteString(w, err.Error())
-			return
-		}
-	}, vbf.MwLogger)
+	// vbf.AddRoute("GET /posts/{postNumber}", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
+	// 	postNumber := r.PathValue("postNumber")
+	// 	mdContent, err := vbf.LoadMarkdown(fmt.Sprintf("./static/docs/%s.md", postNumber))
+	// 	mdContent = StyleMarkdownContent(mdContent)
+	// 	if err != nil {
+	// 		vbf.WriteString(w, "failed to load markdown content")
+	// 		return
+	// 	}
+	// 	err = vbf.ExecuteTemplate(w, t, "layout.html", map[string]interface{}{
+	// 		"Title":       "Posts",
+	// 		"HeaderText":  "Philthy",
+	// 		"SubText":     "Saying the things I'm afraid to say",
+	// 		"Content":     template.HTML(mdContent),
+	// 		"CurrentPath": r.URL.Path,
+	// 	})
+	// 	if err != nil {
+	// 		vbf.WriteString(w, err.Error())
+	// 		return
+	// 	}
+	// }, vbf.MwLogger)
 
-	vbf.AddRoute("GET /posts/{postNumber}", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-		postNumber := r.PathValue("postNumber")
-		mdContent, err := vbf.LoadMarkdown(fmt.Sprintf("./static/docs/%s.md", postNumber))
-		mdContent = StyleMarkdownContent(mdContent)
-		if err != nil {
-			vbf.WriteString(w, "failed to load markdown content")
-			return
-		}
-		t, _ := vbf.GetContext(KEY_TEMPLATES, r).(*template.Template)
-		err = vbf.ExecuteTemplate(w, t, "layout.html", map[string]interface{}{
-			"Title":       "Posts",
-			"HeaderText":  "Philthy",
-			"SubText":     "Saying the things I'm afraid to say",
-			"Content":     template.HTML(mdContent),
-			"CurrentPath": r.URL.Path,
-		})
-		if err != nil {
-			vbf.WriteString(w, err.Error())
-			return
-		}
-	}, vbf.MwLogger)
-
-	err = vbf.Serve(mux, "8080")
+	err := vbf.Serve(mux, "8080")
 	if err != nil {
 		panic(err)
 	}
 }
 
-// ParseTemplatesWithFuncs parses templates and applies a FuncMap to them
-func ParseTemplatesWithFuncs(templateDir string, funcMap template.FuncMap) (*template.Template, error) {
-	// Load and parse templates from the directory
-	tmpl := template.New("").Funcs(funcMap) // Apply the FuncMap globally
-	parsedTmpl, err := tmpl.ParseGlob(templateDir + "/*.html")
-	if err != nil {
-		return nil, err
-	}
-	return parsedTmpl, nil
-}
-
 // takes plain markdown HTML and adds tailwind classes for styling
 func StyleMarkdownContent(mdContent string) string {
-	mdContent = strings.ReplaceAll(mdContent, "<p><em>", "<p class='text-xs italic'><em>")
-	mdContent = strings.ReplaceAll(mdContent, "<h1>", "<h1 class='font-bold text-2xl pt-4'>")
-	mdContent = strings.Replace(mdContent, "<h1 class='font-bold text-2xl pt-4'>", "<h1 class='font-bold text-2xl'>", 1)
-	mdContent = strings.ReplaceAll(mdContent, "<h2>", "<h2 class='font-bold text-xl pt-4'>")
-	mdContent = strings.Replace(mdContent, "<h2 class='font-bold text-xl pt-4'>", "<h2 class='font-bold text-xl'>", 1)
-	mdContent = strings.ReplaceAll(mdContent, "<ol>", "<ol class='list-decimal list-inside pl-4 flex flex-col gap-2'>")
-	mdContent = strings.ReplaceAll(mdContent, "<p><img", "<p class='flex mb-8 items-center'><img class='max-w-sm'")
+	mdContent = strings.Replace(mdContent, "<p><em>", "<p class='text-xs italic pb-8'><em>", 1) // the first <em> is for the date the article was written
+	mdContent = strings.ReplaceAll(mdContent, "<p><em>", "<p class='italic'><em>")              // all the other <em>'s in the markdown file
+	mdContent = strings.ReplaceAll(mdContent, "<h1>", "<h1 class='font-bold text-3xl pt-4 pb-1'>")
+	mdContent = strings.ReplaceAll(mdContent, "<h2>", "<h2 class='font-bold text-2xl pt-8 pb-8'>")
+	mdContent = strings.ReplaceAll(mdContent, "<ol>", "<ol class='list-decimal list-inside pl-4 flex flex-col gap-4 pb-8 pt-4'>")
+	mdContent = strings.ReplaceAll(mdContent, "<p><img", "<p class='flex mb-8 items-center'><img class='max-w-[200px]'")
+	mdContent = strings.ReplaceAll(mdContent, "<p>", "<p class='mb-4'>")
+	mdContent = strings.ReplaceAll(mdContent, "<blockquote", "<blockquote class='italic'")
 
 	return mdContent
 }
@@ -154,12 +106,14 @@ func Layout(title string, headerText string, subText string, currentPath string,
 	    <body class="h-screen">
 	        <div id="root" class="min-h-screen">
 	            <div class="h-[95px]"></div>
-					%s%s%s
+					%s
+     			<div class="h-[65px]"></div>
+					%s%s
 	            <main
-	                class="flex-grow p-4 flex flex-col md:pl-[300px] md:pr-[300px]"
+	                class="flex-grow p-4 flex flex-col md:pl-[300px] lg:pr-[300px] mt-4"
 	                style="min-height: calc(100vh - 190px)"
 	            >
-	                <div class="flex flex-col md:pl-12 md:p-8 gap-4">
+	                <div class="flex flex-col md:pl-12 md:p-8">
 	                    %s
 	                </div>
 	            </main>
@@ -204,11 +158,11 @@ func NavMenu(currentPath string) string {
 	return fmt.Sprintf(`
 		<nav id="navmenu" class="flex top-[95px] fixed bg-white w-2/3 h-full border-r z-40 hidden md:flex md:w-[300px]">
 		    <ul class="flex flex-col gap-2 p-2 w-full text-sm">
-				%s%s%s
+				%s%s
 		    </ul>
 		</nav>
 
-	`, NavItem(currentPath, "/", "Home"), NavItem(currentPath, "/about", "About"), NavItem(currentPath, "/posts", "Posts"))
+	`, NavItem(currentPath, "/", "Home"), NavItem(currentPath, "/posts", "Posts"))
 }
 
 func NavItem(currentPath string, href string, text string) string {
@@ -229,19 +183,14 @@ func NavItem(currentPath string, href string, text string) string {
 
 func SocialMediaBenner() string {
 	return `
-		<div class="bg-black text-white md:pl-[300px] flex flex-row justify-end md:justify-between">
-			<div class='items-center p-4 hidden md:flex'>
-				<p>Support me on other platforms</p>
-			</div>
-			<div>
-				<a href='https://www.youtube.com/@phillip-england' target="_blank">
-					<div class='p-4'>
-						<svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-						  <path fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clip-rule="evenodd"/>
-						</svg>
-					</div>
-				</a>
-			</div>
+		<div class="bg-black text-white md:pl-[300px] flex flex-row fixed top-[95px] h-[65px] w-full">
+			<a href='https://www.youtube.com/@phillip-england' target="_blank">
+				<div class='p-4'>
+					<svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+					  <path fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clip-rule="evenodd"/>
+					</svg>
+				</div>
+			</a>
 
 		</div>
 	`
